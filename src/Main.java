20d04ca86f5a50f3c2e0d956d3667a6b4a79b5cb
s36713 <s36713@pjwstk.edu.pl>
2026-04-13 16:48:20 +0200

    uzycie klas w "main"

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        BiuroUbezpieczen biuro = new BiuroUbezpieczen("Secure Future");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Polisa p1 = new Polisa("CAR-101", "Anna Nowak", 900.0, 3, 72000.0, true, true);
+        Polisa p2 = new Polisa("CAR-102", "Piotr Lis", 840.0, 4, 54000.0, false, false);
+        Polisa p3 = new Polisa("CAR-103", "Karolina Maj", 780.0, 2, 46000.0, true, false);
+
+        biuro.dodajPolisa(p1);
+        biuro.dodajPolisa(p2);
+        biuro.dodajPolisa(p3);
+
+        biuro.wypiszRaport();
+        System.out.println("Laczna skladka: " + biuro.policzLacznaSkladke());
+        System.out.println("Laczna prognoza odnowien: " + biuro.policzLacznaPrognozeOdnowien());
+        System.out.println("Polisy wysokiego ryzyka: " + biuro.policzPolisywysokiegoRyzyka());
+        System.out.println("Liczba utworzonych polis: " + Polisa.getLiczbaUtworzonychPolis());
+
+        System.out.println(p1.pobierzPodsumowanieRyzyka());
+        System.out.println("Prognoza odnowienia dla p1: " + p1.obliczSkladkeKoncowa());
+        System.out.println(p1.equals(new Polisa("CAR-101", "Inny klient", 700.0, 1, 30000.0, false, false)));
+        System.out.println(p2);
+
+        Polisa znaleziona = biuro.znajdzPoNumerze("CAR-102");
+        System.out.println("Znaleziono: " + znaleziona);
     }
 }
